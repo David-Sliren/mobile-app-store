78dad84ce8d925a45f373629bd8b9d05dbb44e1d
David Sliren <bluee.97.berry@gmail.com>
2026-03-10 16:07:38 -0500

    feact(home): Agregar estado en el componente FeatureCard, para indicar cuando una funcionalidad no esta disponible

--- src/components/Home/FeatureCard.jsx
+++ src/components/Home/FeatureCard.jsx
@@ -1,17 +1,23 @@
 import { motion } from "motion/react";
 import { fadeIn } from "../../constants/motion_variants";
 
-const FeatureCard = ({ icon: Icon, title, desc }) => (
-  <motion.div
+const FeatureCard = ({ icon: Icon, title, desc, isSoon = false }) => (
+  <motion.button
+    disabled={isSoon}
     variants={fadeIn}
-    className="p-8 rounded-3xl bg-zinc-900 border border-zinc-800 hover:border-emerald-500/50 transition-colors group cursor-pointer"
+    className={`relative p-8 rounded-3xl bg-zinc-900 border border-zinc-800 hover:border-emerald-500/50 transition-colors group ${isSoon ? "cursor-not-allowed" : "cursor-pointer"}`}
   >
+    {isSoon && (
+      <div className="absolute top-4 right-4 size-fit bg-emerald-500/6 flex items-center justify-center border border-emerald-500/20 px-2 rounded-full group-hover:bg-emerald-500/10  transition-colors text-md sm:text-sm md:text-md xl:text-lg text-emerald-500 font-semibold">
+        Proximamente
+      </div>
+    )}
     <div className="w-12 h-12 rounded-2xl bg-zinc-800 flex items-center justify-center mb-6 group-hover:bg-emerald-500/10 transition-colors">
       <Icon className="text-emerald-500 size-6" />
     </div>
     <h3 className="text-white text-xl font-bold mb-3">{title}</h3>
     <p className="text-zinc-500 leading-relaxed text-sm">{desc}</p>
-  </motion.div>
+  </motion.button>
 );
 
 export default FeatureCard;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -23,16 +23,20 @@ const Home = () => {
           className="grid grid-cols-1 md:grid-cols-3 gap-8"
         >
           <FeatureCard
-            icon={Zap}
-            title="Nuevas versiones"
-            desc="¡Lo último que he creado! Disfruta de las funciones más recientes con una rapidez que te va a encantar en cada toque."
-          />
-          <FeatureCard
             icon={ShieldCheck}
             title="Versiones estables"
             desc="Tus favoritas, listas para usar. Aplicaciones probadas a fondo para que funcionen siempre perfecto."
           />
+
           <FeatureCard
+            isSoon={true}
+            icon={Zap}
+            title="Nuevas versiones"
+            desc="¡Lo último que he creado! Disfruta de las funciones más recientes con una rapidez que te va a encantar en cada toque."
+          />
+
+          <FeatureCard
+            isSoon={true}
             icon={Rocket}
             title="Versiones beta"
             desc="Sé parte del proceso. Prueba antes que nadie mis experimentos y ayúdame a mejorarlos con tus ideas..."
